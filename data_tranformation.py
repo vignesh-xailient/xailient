122893423f1152d901963f8e51dc1ff02e494e7d
vignesh-xailient <65212104+vignesh-xailient@users.noreply.github.com>
2020-05-20 17:08:48 +1000

    Update data_tranformation.py

--- data_tranformation.py
+++ data_tranformation.py
@@ -12,4 +12,8 @@ df= df.drop(df.columns[3], axis=1)
 
 df = df.dropna()                               #removal of null rows
 
+df[['del1','del2','file_name']] = df.file_name.str.split("_",n=3,expand=True)     #spliting of filename,removal of the file path name
+df= df.drop(df.columns[0], axis=1)                                        #removal of del1 and del2
+df= df.drop(df.columns[0], axis=1)
+
 df.to_csv('data2.csv',index = False)
